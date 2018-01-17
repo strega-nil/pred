04fcd49b300a8f8688a93363332f030d02ddee10
nicole mazzuca <npmazzuca@gmail.com>
2018-01-16 18:00:50 -0800

    add more iterator transformers, standardize naming

--- pred/iter.ml
+++ pred/iter.ml
@@ -1,17 +1,147 @@
 type 'a t =
 | Iter: 'state * ('state -> ('state * 'a) option) -> 'a t
 
-let for_each f (Iter (state, iter)) =
+
+(* constructors *)
+
+let make state func = Iter (state, func)
+
+let empty = Iter ((), fun () -> None)
+
+let once el =
+  let state = Some el in
+  let func = function
+  | Some el -> Some (None, el)
+  | None -> None
+  in
+  Iter (state, func)
+
+let repeat el =
+  let state = el in
+  let func el = Some (el, el) in
+  Iter (state, func)
+
+let range init fin =
+  let state = (init, fin) in
+  let func (cur, fin) =
+    if cur < fin then
+      Some ((cur + 1, fin), cur + 1)
+    else
+      None
+  in
+  Iter (state, func)
+
+
+(* transformations *)
+
+let map f (Iter (state, func')) =
+  let func state =
+    match func' state with
+    | Some (state, el) -> Some (state, f el)
+    | None -> None
+  in
+  Iter (state, func)
+
+let flatten (Iter (main_state, main_func)) =
+  match main_func main_state with
+  | Some (main_state, iter) ->
+    let state = (iter, main_state) in
+    let rec func (Iter (iter_state, iter_func), main_state) =
+      match iter_func iter_state with
+      | Some (iter_state, el) ->
+        Some ((Iter (iter_state, iter_func), main_state), el)
+      | None ->
+        (match main_func main_state with
+        | Some (main_state, iter) ->
+          func (iter, main_state)
+        | None -> None)
+    in
+    Iter (state, func)
+  | None -> empty
+
+exception Iter_zipped_iterators_of_different_lengths
+let zip (Iter (state1, func1)) (Iter (state2, func2)) =
+  let state = (state1, state2) in
+  let func (state1, state2) =
+    match (func1 state1, func2 state2) with
+    | (Some (state1, el1), Some (state2, el2)) ->
+      Some ((state1, state2), (el1, el2))
+    | (None, None) -> None
+    | _ -> raise Iter_zipped_iterators_of_different_lengths
+  in
+  Iter (state, func)
+
+let enumerate (Iter (state', func')) =
+  let state = (0, state') in
+  let func (i, state') =
+    match (func' state') with
+    | Some (state', el') -> Some ((i + 1, state'), (i + 1, el'))
+    | None -> None
+  in
+  Iter (state, func)
+
+
+(* misc *)
+
+let take final (Iter (state', func')) =
+  let state = (0, state') in
+  let func (n, state') =
+    if n < final then
+      match func' state' with
+      | Some (state', el) -> Some((n + 1, state'), el)
+      | None -> None
+    else
+      None
+  in
+  Iter (state, func)
+
+let take_while pred (Iter (state, func')) =
+  let func state =
+    match func' state with
+    | Some (state, el) when pred el -> Some (state, el)
+    | Some _
+    | None -> None
+  in
+  Iter (state, func)
+
+
+(* use *)
+
+let next (Iter (state, func)) =
+  match func state with
+  | Some (new_state, el) -> Some (Iter (new_state, func), el)
+  | None -> None
+
+let nth n (Iter (state, func)) =
+  let rec helper n state =
+    match func state with
+    | Some (state, _) when n > 0 -> helper (n - 1) state
+    | Some (_, el) -> Some el
+    | None -> None
+  in
+  helper n state
+
+let fold f init (Iter (state, func)) =
+  let rec helper cur state =
+    match func state with
+    | Some (state, el) ->
+      let cur = f el in
+      helper cur state
+    | None -> cur
+  in
+  helper init state
+
+let for_each f (Iter (state, func)) =
   let rec helper state =
-    match iter state with
+    match func state with
     | Some (state, el) -> f(el); helper(state)
     | None -> ()
   in
   helper state
 
-let for_each_break f (Iter (state, iter)) =
+let for_each_break f (Iter (state, func)) =
   let rec helper state =
-    match iter state with
+    match func state with
     | Some((state, el)) -> (
       match f(el) with
       | Some(ret) -> Some(ret)
@@ -20,84 +150,13 @@ let for_each_break f (Iter (state, iter)) =
   in
   helper(state)
 
-let next (Iter (state, func)) =
-  match func state with
-  | Some (new_state, el) -> Some (Iter (new_state, func), el)
-  | None -> None
-
-let make state func = Iter (state, func)
-
-let empty = Iter ((), fun () -> None)
-
-let map f (Iter (state, iter')) =
-  let iter state =
-    match iter' state with
-    | Some (state, el) -> Some (state, f el)
-    | None -> None
-  in
-  Iter (state, iter)
-
-let flatten (Iter (main_state, main_iter)) =
-  match main_iter main_state with
-  | Some (main_state, cur_iter) ->
-    let state = (cur_iter, main_state) in
-    let rec iter (Iter (iter_state, iter_iter), main_state) =
-      match iter_iter iter_state with
-      | Some (iter_state, el) ->
-        Some ((Iter (iter_state, iter_iter), main_state), el)
-      | None ->
-        (match main_iter main_state with
-        | Some (main_state, cur_iter) ->
-          iter (cur_iter, main_state)
-        | None -> None)
-    in
-    Iter (state, iter)
-  | None -> empty
-
-exception Iter_zipped_iterators_of_different_lengths
-let zip (Iter (state1, iter1)) (Iter (state2, iter2)) =
-  let state = (state1, state2) in
-  let iter (state1, state2) =
-    match (iter1 state1, iter2 state2) with
-    | (Some (state1, el1), Some (state2, el2)) ->
-      Some ((state1, state2), (el1, el2))
-    | (None, None) -> None
-    | _ -> raise Iter_zipped_iterators_of_different_lengths
-  in
-  Iter (state, iter)
-
-let enumerate (Iter (state', iter')) =
-  let state = (0, state') in
-  let iter (i, state') =
-    match (iter' state') with
-    | Some (state', el') -> Some ((i + 1, state'), (i + 1, el'))
-    | None -> None
-  in
-  Iter (state, iter)
-
-let range init fin =
-  let state = (init, fin) in
-  let iter (cur, fin) =
-    if cur < fin then
-      Some ((cur + 1, fin), cur + 1)
-    else
-      None
-  in
-  Iter (state, iter)
-
 module Monad = struct
   type nonrec 'a t = 'a t
 
-  let wrap x =
-    let state = Some x in
-    let iter = function
-    | Some x -> Some (None, x)
-    | None -> None
-    in
-    Iter (state, iter)
+  let wrap = once
 
   let (>>=) iter f = flatten (map f iter)
-      
+
   module Let_syntax = struct
     let bind x ~f = x >>= f
   end
--- pred/iter.mli
+++ pred/iter.mli
@@ -1,5 +1,27 @@
 type 'a t
 
+(* constructors *)
+val make: 'state -> ('state -> ('state * 'a) option) -> 'a t
+val empty: 'a t
+val once: 'a -> 'a t
+val repeat: 'a -> 'a t
+val range: int -> int -> int t
+
+(* transformations *)
+val map: ('a -> 'b) -> 'a t -> 'b t
+val flatten: 'a t t -> 'a t
+exception Iter_zipped_iterators_of_different_lengths
+val zip: 'a t -> 'b t -> ('a * 'b) t
+val enumerate: 'a t -> (int * 'a) t
+
+(* misc *)
+val take: int -> 'a t -> 'a t
+val take_while: ('a -> bool) -> 'a t -> 'a t
+
+(* use *)
+val next: 'a t -> ('a t * 'a) option
+val nth: int -> 'a t -> 'a option
+val fold: ('a -> 'a) -> 'a -> 'a t -> 'a
 val for_each: ('a -> unit) -> 'a t -> unit
 
 (**
@@ -7,17 +29,5 @@ val for_each: ('a -> unit) -> 'a t -> unit
   else, if the inner closure returns None for all iterated values, return None
  *)
 val for_each_break: ('a -> 'b option) -> 'a t -> 'b option
-
-val next: 'a t -> ('a t * 'a) option
-
-val make: 'state -> ('state -> ('state * 'a) option) -> 'a t
-val empty: 'a t
-val map: ('a -> 'b) -> 'a t -> 'b t
-val flatten: 'a t t -> 'a t
-
-exception Iter_zipped_iterators_of_different_lengths
-val zip: 'a t -> 'b t -> ('a * 'b) t
-val enumerate: 'a t -> (int * 'a) t
-val range: int -> int -> int t
 
 module Monad: Interfaces.Monad with type 'a t = 'a t
