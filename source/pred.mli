bc4d508c3d25e7690e67c3af7e5dc5fd0fb14f30
nicole mazzuca <npmazzuca@gmail.com>
2018-02-02 17:49:01 -0800

    bsb support

--- source/pred.mli
+++ source/pred.mli
@@ -25,15 +25,16 @@
   to the [library] or [executables] section.
 *)
 
-module Array = Array
-module Interfaces = Interfaces
-module List = List
-module Option = Option
-module Result = Result
-module Seq = Seq
-module String_buffer = String_buffer
-module Caml = Pred_caml_stdlib
+module Array = Pred_array
+module Interfaces = Pred_interfaces
+module List = Pred_list
+module Option = Pred_option
+module Result = Pred_result
+module Seq = Pred_seq
+module String_buffer = Pred_string_buffer
+module Caml = Pred_caml
 
 type 'a seq = 'a Seq.t
 
 type string_buffer = String_buffer.t
+type ('o, 'e) result = ('o, 'e) Result.t
